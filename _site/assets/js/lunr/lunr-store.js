b3def6672b15345ea879d6c8ea023fcab70e4dd4
Jee-Hyung Son <sonji0988@gmail.com>
2023-10-04 09:53:14 +0900

    comments test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,25 +1,7 @@
 var store = [{
-        "title": "[CS]",
-        "excerpt":"CS  ","categories": ["CS"],
-        "tags": [],
-        "url": "/cs/CS-1/",
-        "teaser": null
-      },{
-        "title": "[BOJ]",
-        "excerpt":"백준  ","categories": ["Coding Test"],
+        "title": "댓글 테스트",
+        "excerpt":"백준  댓글 테스트용 포스팅  ","categories": ["Coding Test"],
         "tags": [],
         "url": "/coding%20test/BOJ-1234/",
-        "teaser": null
-      },{
-        "title": "[이산수학]",
-        "excerpt":"이산 수학   ","categories": ["Math"],
-        "tags": [],
-        "url": "/math/discrete-mathematics-1/",
-        "teaser": null
-      },{
-        "title": "[Python]",
-        "excerpt":"파이썬  ","categories": ["Python"],
-        "tags": [],
-        "url": "/python/python-1/",
         "teaser": null
       }]
