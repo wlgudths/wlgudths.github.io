21ed37aefd2dbdab5b0617f2f6c564a14ed40803
손지형 <sonjeehyung@sonjihyeong-ui-iMac.local>
2023-10-15 01:51:16 +0900

    Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/coding%20test/BOJ-1234/",
         "teaser": null
+      },{
+        "title": "[이산수학] 이산수학(Discrete Mathmatics)",
+        "excerpt":"이산수학이란? 이산수학(discrete mathematics)은 이산적인 수학 구조에 대해 연구하는 학문으로, 연속되지 않는 공간을 다룬다. 유한 수학 또는 전산 수학이라고 한다. 이산수학에서는 실수, 미적분학같이 연속적인(continuous) 성질이 있는 대상이 아니라 주로 정수, 그래프, 논리 연산같이 서로 구분되는 값을 가지는 대상으로 연구한다. 예시 이산수학을 왜 배울까? 컴퓨터는 0과 1의 조합으로 데이터를 저장하기 때문에 이산...","categories": ["Math"],
+        "tags": [],
+        "url": "/math/MATH-01/",
+        "teaser": null
       }]
